fca9a2165eff834ee916b5b86a4eaa7e5e41f485
Edward Łuska <edi@nabi.pl>
2019-03-22 18:25:25 +0100

    Config switched to "production mode"

--- src/config/top-coin.config.js
+++ src/config/top-coin.config.js
@@ -1,4 +1,4 @@
 export default {
-  // url: 'http://nabi.pl/test/cmc_proxy/cmc_proxy.php',
-  url: 'http://nabi.pl/test/cmc_proxy/cmc_proxy_cache.php',
+  url: 'http://nabi.pl/test/cmc_proxy/cmc_proxy.php',
+  // url: 'http://nabi.pl/test/cmc_proxy/cmc_proxy_cache.php',
 };
